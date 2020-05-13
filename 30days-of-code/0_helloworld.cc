4aa27956ac4a3500bb456c630de7fe7dd1ddc0e1
RawIron <manianology@gmail.com>
2020-05-13 16:44:11 -0700

    Clean helloworld cc

--- 30days-of-code/0_helloworld.cc
+++ 30days-of-code/0_helloworld.cc
@@ -1,12 +1,9 @@
 int main() {
-   string inputString; // declare a variable to hold our input
-   getline(cin, inputString); // get a line of input from cin and save it to our variable
+   string message{};
+   getline(cin, message);
   
-   // Your first line of output goes here
    cout << "Hello, World." << endl;
-
-   // Write the second line of output
-   cout << inputString << endl;
+   cout << message << endl;
 
    return 0;
 }
