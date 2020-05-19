8aab75f905eaca2148d7d4054eaa90cd606813e3
RawIron <manianology@gmail.com>
2020-05-18 21:45:45 -0700

    Search only up to sqrt of n in interfaces py

--- 30days-of-code/19_interfaces.py
+++ 30days-of-code/19_interfaces.py
@@ -1,35 +1,51 @@
+import math
+
 class AdvancedArithmetic(object):
     def divisor_sum(self, number):
         raise NotImplementedError
 
 
 class Calculator(AdvancedArithmetic):
-    def divisor_sum(self, number):
+    def divisor_sum(self, n):
         if n == 1:
             return 1
 
         # an even number is divisible by even and uneven numbers
         # an uneven number is only divisible by uneven numbers
         n_is_even = (n % 2 == 0)
+        if n_is_even:
+            begin = 2
+            step = 1
+        else:
+            begin = 3
+            step = 2
 
-        begin = n // 2
-        step = -1
-        if not n_is_even:
-            step = -2
-            # 9 // 2 == 4
-            # only uneven numbers can divide 9 so begin at 3
-            n_div2_is_even = (n // 2 % 2 == 0)
-            if n_div2_is_even:
-                begin -= 1
+        # sqrt(6) = 2.45
+        # ceil(2.45) = 3
+        # only search up to 2
+        # thus range upper bound in loop should be 3
+        sqrt_n = math.sqrt(n)
+        ceil_sqrt_n = math.ceil(sqrt_n)
 
         # every n can be divided by 1 and by itself
         div_sum = n + 1
-        for divisor in range(begin, 1, step):
+        # square root is a divisor of n
+        # add it only once
+        if sqrt_n == ceil_sqrt_n and n % sqrt_n == 0:
+            # sqrt(9) = 3
+            # ceil_sqrt(3) = 3
+            div_sum += sqrt_n
+
+        # devisors are communatative
+        #    6 // 2 == 3
+        # => 6 // 3 == 2
+        # 2,3 are divisors
+        for divisor in range(begin, ceil_sqrt_n, step):
             if (n % divisor) == 0:
                 div_sum += divisor
+                div_sum += n // divisor
 
         return div_sum
-
 
 def solve(number):
     my_calculator = Calculator()
