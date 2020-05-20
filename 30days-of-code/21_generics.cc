1bbea5ca1141d198175fb62756cd56c59a6a115a
RawIron <manianology@gmail.com>
2020-05-20 10:54:11 -0700

    30 days of code - day 21 cc

---
+++ 30days-of-code/21_generics.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+template<typename T>
+void printArray(const vector<T> items) {
+    for(auto item : items) {
+        cout <<item <<endl;
+    }
+}
+
+
+template<typename T>
+vector<T> readFixedMany() {
+	size_t n{};
+	cin >> n;
+
+	vector<T> in_vector(n);
+	for (size_t i{0}; i < n; ++i) {
+		T value;
+		cin >> value;
+		in_vector[i] = value;
+	}
+    return in_vector;
+}
+
+
+int main() {
+    vector<int> int_vector{ readFixedMany<int>() };
+    vector<string> string_vector{ readFixedMany<string>() };
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
+}
