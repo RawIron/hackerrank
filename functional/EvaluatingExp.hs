a80ee803b76ac0de950fa75f3fcda2969eaa000f
RawIron <manianology@gmail.com>
2021-03-01 16:43:56 -0800

    Functional Programming - evaluating e^x; changed the input function

--- functional/EvaluatingExp.hs
+++ functional/EvaluatingExp.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Monad
 import Text.Printf
 
+
 fact :: Int -> Int
 fact n = product [2 .. n] 
 
@@ -13,9 +14,28 @@ exproximate n x = (x^n / (fromIntegral . fact) n) + exproximate (n-1) x
 myexp :: Double -> Double
 myexp x = exproximate 9 x
 
-main :: IO()
+
+input :: IO [Double]
+input = do
+  n <- readLn :: IO Int
+  inputSeq' n
+
+-- use sequence
+inputSeq' :: Int -> IO [Double]
+inputSeq' n = sequence $ replicate n (readLn :: IO Double)
+
+-- an implementation of sequence using recursion
+-- the implementation in base is
+--  sequence = mapM id
+input' :: Int -> IO [Double]
+input' 0 = return []
+input' n = do
+  x <- readLn :: IO Double
+  xs <- input' (n-1)
+  return (x: xs)
+
+
+main :: IO ()
 main = do
-    n <- readLn :: IO Int
-    forM_ [1..n] $ \n_itr -> do
-        x <- readLn :: IO Double
-        printf "%.4f\n" . myexp $ x
+  numbers <- input
+  mapM_ (printf "%.4f\n" . myexp) numbers
