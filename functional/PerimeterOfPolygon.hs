386fe62e097a8fb13f9623b0c652c0efcbecb53f
RawIron <manianology@gmail.com>
2021-03-02 14:31:33 -0800

    Functional Programming - perimeter of polygon; cleaner

--- functional/PerimeterOfPolygon.hs
+++ functional/PerimeterOfPolygon.hs
@@ -6,15 +6,23 @@ euclid_distance :: Point -> Point -> Double
 euclid_distance (p1, p2) (q1, q2) = sqrt . fromIntegral $ (p1 - q1)^2 + (p2 - q2)^2
 
 solve :: [Point] -> Double
-solve points = sum [euclid_distance x y |  (x,y) <- zip points (tail points ++ [head points])]
+solve points =
+  sum [euclid_distance x y | (x,y) <- zip points (tail points ++ [head points])]
 
 
-parsePairs :: [Int] -> [(Int, Int)]
-parsePairs [] = []
-parsePairs (x:y:xs) = (x,y) : parsePairs xs
+-- identical to sliceVertPair implementation
+slicePairs :: [Int] -> [(Int, Int)]
+slicePairs = go where
+  go (x:y:xs) = (x,y) : go xs
+  go [] = []
+  go _ = []
+
+input :: IO [(Int, Int)]
+input = do
+  n <- readLn :: IO Int
+  getContents >>= return . slicePairs . map read . words
+
 
 main :: IO ()
 main = do
-  n <- readLn :: IO Int
-  input <- getContents
-  putStrLn . show . solve . parsePairs $ (map read (words input) :: [Int])
+  input >>= putStrLn . show . solve
