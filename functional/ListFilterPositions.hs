3a1ed8deb1dd4c0c792cdaf38bf810405e02e8b9
RawIron <manianology@gmail.com>
2021-03-07 12:18:11 -0800

    Functional Programming - list filter positions; cleaner

--- functional/ListFilterPositions.hs
+++ functional/ListFilterPositions.hs
@@ -1,10 +1,9 @@
 module ListFilterPositions where
 
 
-f :: [Int] -> [Int]
-f lst = map (snd) . filter ((0 ==) . flip mod 2 . fst) . zip [1..length lst] $ lst
+evenPositions :: [Int] -> [Int]
+evenPositions lst = map snd . filter ((0 ==) . flip mod 2 . fst) . zip [1..length lst] $ lst
 
 main :: IO ()
 main = do
-	inputdata <- getContents
-	mapM_ (putStrLn . show) . f . map read . lines $ inputdata
+  getContents >>= mapM_ (putStrLn . show) . evenPositions . map read . lines
