8663d21fe2156473105224d758e35d96945f1bee
RawIron <manianology@gmail.com>
2021-03-12 12:49:37 -0800

    Functional Programming - string mingling; readability

--- functional/StringMingling.hs
+++ functional/StringMingling.hs
@@ -1,13 +1,22 @@
 module Main where
 
-pairify :: [a] -> (a,a)
-pairify [x,y] = (x,y)
-
+-- | combine two words into one like a zipper
+--
+-- b_l_a_c_k
+-- _f_o_r_e_s_t
+-- bfloarceks
 mingle :: String -> String -> String
-mingle [] [] = []
+mingle [] _ = []
+mingle _ [] = []
 mingle fstWord sndWord =
     (head fstWord) : (head sndWord) : mingle (tail fstWord) (tail sndWord)
 
+pairify :: [a] -> (a,a)
+pairify [x,y] = (x,y)
+
+input :: IO (String, String)
+input = getContents >>= return . pairify . words
+
 main :: IO ()
 main = do
-  getContents >>= putStrLn . uncurry mingle . pairify . words
+  input >>= putStrLn . uncurry mingle
