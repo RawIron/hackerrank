288b9db785d67ea0e513c544cce9e511acb6bcdd
RawIron <manianology@gmail.com>
2021-03-12 20:48:39 -0800

    Functional Programming - area of polygon; readability

--- functional/AreaOfPolygon.hs
+++ functional/AreaOfPolygon.hs
@@ -5,9 +5,13 @@ type Point = (Int, Int)
 combine :: Point -> Point -> Int
 combine (p1, p2) (q1, q2) = (p1 * q2) - (p2 * q1)
 
+-- | change path encoding from (p1-p2-p3-..-pn) to (p1-p2,p2-p3,p3-p4,..,pn-1-pn)
+pathAsPairs :: [(Int, Int)] -> [((Int, Int), (Int, Int))]
+pathAsPairs points = zip points (tail points ++ [head points])
+
 solve :: [Point] -> Double
 solve points =
-  (abs . (/2) . fromIntegral . sum) [combine x y | (x,y) <- zip points (tail points ++ [head points])]
+  (abs . (/2) . fromIntegral . sum) [combine x y | (x,y) <- pathAsPairs points]
 
 
 -- identical to sliceVertPair implementation
