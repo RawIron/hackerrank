674fb7b5091f4e5c347d8119fc7296f23062682f
RawIron <manianology@gmail.com>
2021-03-22 22:05:23 -0700

    python skills - piling up; readability

--- python-skills/piling_up.py
+++ python-skills/piling_up.py
@@ -1,28 +1,39 @@
-import sys
-
-t = int(input())
-
-for _ in range(0,t):
-    n = int(input())
-    lengths = list(map(int, input().split()))
-    
-    last = sys.maxsize
+def solve(lengths):
+    last = 2**31
     l = 0
     r = len(lengths)-1
-    
-    while (l <= r):        
+
+    while l <= r:
         if lengths[l] >= lengths[r]:
             picked = lengths[l]
             l += 1
         else:
             picked = lengths[r]
             r -= 1
-        
+
         if picked > last:
             break
+
         last = picked
-        
+
     if l <= r:
-        print("No")
+        return False
     else:
-        print("Yes")
+        return True
+
+def read():
+    n = int(input())
+    return list(map(int, input().split()))
+
+def main():
+    tests = int(input())
+    for _ in range(0, tests):
+        lengths = read()
+
+        can_be_solved = solve(lengths)
+        if can_be_solved:
+            print("Yes")
+        else:
+            print("No")
+
+main()
