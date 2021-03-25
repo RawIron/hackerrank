37dea209190d900e053a2b1b4ca7c2ad26965f14
RawIron <manianology@gmail.com>
2021-03-25 10:40:51 -0700

    python skills - piling up; haskell comments

--- python-skills/PilingUp.hs
+++ python-skills/PilingUp.hs
@@ -2,6 +2,18 @@ module Main (
   main
 ) where
 
+{- | can smaller bumper plates always be stacked on larger bumper plates
+ - | until there are no plates left on the bar?
+ -  a bar has several bumper plates of different weights/diameter
+ -      -4-6-8-3-1-
+ -  in the above example the plate "4" or the plate "1" can be removed from the bar
+ -
+ -  on every move take the larger of the two accessable bumper plates from the bar
+ -  and put it on top of the stacked plates
+ -
+ - > pileUp [4,6,8,3,1] == False
+ - > pileUp [5,4,1]] == True
+ -}
 pileUp :: [Int] -> Bool
 pileUp plates = go emptyStack plates
   where
@@ -21,11 +33,24 @@ pileUp plates = go emptyStack plates
     putRightOnStack = (snd stack, last plates)
     removeRight = (init plates)
 
+
+-- | print a bool value as Yes/No
+-- this is not a good solution
+-- use composition instead
 printAnswer :: Bool -> IO ()
 printAnswer canBeSolved
   | canBeSolved = putStrLn "Yes"
   | otherwise = putStrLn "No"
 
+-- | print a bool value as Yes/No
+-- (putStrLn . show . bool2YesNo)
+data YesNo = Yes | No deriving (Show)
+
+bool2YesNo :: Bool -> YesNo
+bool2YesNo isTrue
+  | isTrue = Yes
+  | otherwise = No
+
 main :: IO ()
 main = do
-  printAnswer $ pileUp [7,4,6]
+  (putStrLn . show . bool2YesNo . pileUp) $ [7,4,6]
