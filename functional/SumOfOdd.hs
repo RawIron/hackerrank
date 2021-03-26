302ddc4b10a125bd885d5c02f2082f93048329f4
RawIron <manianology@gmail.com>
2021-03-26 16:58:57 -0700

    Functional Programming - sum of odds, readability

--- functional/SumOfOdd.hs
+++ functional/SumOfOdd.hs
@@ -1,9 +1,10 @@
 module SumOfOdd where
 
-
+-- | sum odd numbers in an array
+-- > f [1,2,4,5] == 6
 f :: [Int] -> Int
 f arr = sum . filter (not . even) $ arr
 
+main :: IO ()
 main = do
-  inputdata <- getContents
-  putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
+  getContents >>= putStrLn . show . f . map read . lines
