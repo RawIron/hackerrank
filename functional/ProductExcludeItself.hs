b79cfc0db17ef61ae80dd6e798b4a7c8c622ac6c
RawIron <manianology@gmail.com>
2021-03-26 19:25:32 -0700

    Functional Programming - product without itself; add comments

--- functional/ProductExcludeItself.hs
+++ functional/ProductExcludeItself.hs
@@ -1,5 +1,8 @@
 module Main where
 
+-- | multiply the non-zero elements of a list
+-- | and collect the positions of the zeros
+-- > prodDiffZeros [1,2,0,3,0] == (6,[3,5])
 prodDiffZeros :: [Int] -> (Int, [Int])
 prodDiffZeros numbers = (areAllZero numbers) $ calcProdAndZeros numbers
   where
@@ -12,6 +15,10 @@ prodDiffZeros numbers = (areAllZero numbers) $ calcProdAndZeros numbers
       | number == 0 = (product, index : zeros)
       | otherwise = (number * product, zeros)
 
+-- | elemet in the result list is
+-- | the product of the list passed in divided by the element
+-- > prodWithoutItself [3,4,1] == [4,3,12]
+-- > prodWithoutItself [3,4,0] == [0,0,12]
 prodWithoutItself :: [Int] -> [Int]
 prodWithoutItself numbers = (calcProduct numbers) $ prodDiffZeros numbers
   where
@@ -27,13 +34,16 @@ prodWithoutItself numbers = (calcProduct numbers) $ prodDiffZeros numbers
     zeroIndex = head zeros
     n = length numbers
 
-test_prodWithoutItself :: [[Int]]
-test_prodWithoutItself = foldl go [] tests
+-- | tests
+testProdWithoutItself :: [[Int]]
+testProdWithoutItself = foldl go [] tests
   where
   tests = [([1,2,3], [6,3,2]),
+           ([1,1,1], [1,1,1]),
            ([0,0,0], [0,0,0]),
            ([0,2,0], [0,0,0]),
            ([5,2,0], [0,0,10]),
+           ([5,0,1], [0,5,0]),
            ([0,4,3], [12,0,0])
           ]
   go failedTests test
@@ -44,4 +54,4 @@ test_prodWithoutItself = foldl go [] tests
 
 main :: IO ()
 main = do
-  mapM_ (putStrLn . show) $ test_prodWithoutItself
+  mapM_ (putStrLn . show) $ testProdWithoutItself
