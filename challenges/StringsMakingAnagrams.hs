2a098915fe5ad35f29ced195ed922a94611d655a
RawIron <manianology@gmail.com>
2021-04-01 16:52:20 -0700

    challenges: strings making anagrams; comments

--- challenges/StringsMakingAnagrams.hs
+++ challenges/StringsMakingAnagrams.hs
@@ -11,6 +11,11 @@ import System.IO
 --  bacdc and dcbac are anagrams
 -- only operation allowed is delete a character
 --
+-- remove from both strings the characters they have in common
+--  fgabdcc \\ (fgabdcc intersect daabc) == fgc
+--  daabc \\ (fgabdcc intersect daabc) == a
+-- count the characters in the result
+--
 -- > makeAnagram "dab" "bac" == 2
 makeAnagram :: String -> String -> Int
 makeAnagram fstWord sndWord
@@ -18,33 +23,40 @@ makeAnagram fstWord sndWord
     | otherwise = length $ lettersNotShared sndWord fstWord
 
 -- |
+-- > lettersNotShared "dab" "bac" == "cd"
+-- > lettersNotShared "fgabdcc" "daabc" == "fgca"
 lettersNotShared :: String -> String -> String
 lettersNotShared shortWord longWord = mergeLetters
     where
-    mergeLetters = (freqToList deleteFromLong) ++ deleteFromShort
-    (deleteFromLong, deleteFromShort) = foldl go (frequencies longWord, []) shortWord
+    mergeLetters = (freqToList notSharedLong) ++ notSharedShort
+    (notSharedLong, notSharedShort) = foldl go (frequencies longWord, []) shortWord
     go (searchThis, notFound) letter
-        | wasFound = (decrOrDelete, notFound)
+        | wasFound = (freqDecr letter searchThis, notFound)
         | otherwise = (searchThis, letter : notFound)
         where
         wasFound = Map.member letter searchThis
-        decrOrDelete = freqDecr letter searchThis
 
--- |
+-- | build frequency table of the characters in a string
+-- > frequencies "halla" == Map.fromList [('a',2),('h',1),('l',2)]
 frequencies :: String -> Map Char Int
 frequencies str = Map.fromListWith (+) $ zip str (repeat 1)
 
+-- | decrement the count of a key in a frequency table
+-- > freqDecr 'a' Map.fromList [('a',2),('h',1)] == Map.fromList [('a',1),('h',1)]
+-- > freqDecr 'a' Map.fromList [('a',1),('h',1)] == Map.fromList [('h',1)]
 freqDecr :: Char -> Map Char Int -> Map Char Int
 freqDecr k freqTable = Map.update f k freqTable
     where
     f x = if x > 1 then Just (x-1) else Nothing
-    
+
+-- | characters in their frequencies combined into a string
+-- > freqToList Map.fromList [('a',2),('h',1),('l',2)] == "aahll"
 freqToList :: Map Char Int -> String
 freqToList freq = Map.foldrWithKey go [] freq
     where
     go k v acc = (take v $ repeat k) ++ acc
 
--- |
+-- | convert list with two elements into a pair
 pairify :: [a] -> (a,a)
 pairify [x,y] = (x,y)
 
