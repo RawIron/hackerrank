e79a6efd641e59321ba4e8fd1b4ab2a5e2b77654
RawIron <manianology@gmail.com>
2021-04-30 14:16:04 -0700

    Functional Programming - Fibonacci; fix bug

--- functional/Fibonacci.hs
+++ functional/Fibonacci.hs
@@ -1,9 +1,12 @@
 module Main where
 
+-- | the n-th Fibonacci Number
+-- > fib 4 == 3
+-- > fib 11 == 89
 fib :: Int -> Int
-fib 1 = 0
-fib 2 = 1
-fib n = fst . foldl (\(p, q) _ -> (p+q, p)) (1, 0) $ [3..n]
+fib 0 = 0
+fib 1 = 1
+fib n = fst . foldl (\(p, q) _ -> (p+q, p)) (1, 0) $ [2..n]
 
 main = do
   getLine >>= print . fib . (read :: String -> Int)
