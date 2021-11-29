0359569dd2b0798c2bcd5a129ee21d6e22000513
RawIron <manianology@gmail.com>
2021-11-29 10:10:42 -0800

    challenges: modified kaprekar numbers, improve readability

--- challenges/KaprekarNumbers.hs
+++ challenges/KaprekarNumbers.hs
@@ -5,23 +5,26 @@ import qualified Data.Text as T
 -- | split a number after n digits counted
 -- | from the right
 -- > split 4 123456 == (12,3456)
+-- > split 3 123 == (0,123)
 split :: Int -> Int -> (Int, Int)
 split d number
-  | numberSize == d = (0, number)
-  | otherwise = fromRight
+  | digitsInNumber == d = (0, number)
+  | otherwise = (remaining, countedDigits)
   where
   numberText = T.pack . show $ number
-  numberSize = T.length numberText
+  digitsInNumber = T.length numberText
   take n t = (read . T.unpack . T.take n) $ t
   drop n t = (read . T.unpack . T.drop n) $ t
-  fromRight = (take (numberSize - d) numberText, drop (numberSize - d) numberText)
+  remaining = take (digitsInNumber - d) numberText
+  countedDigits = drop (digitsInNumber - d) numberText
 
 -- | is the number a Kaprekar Number
 -- > isKaprekar 99 == True
 isKaprekar :: Int -> Bool
 isKaprekar number = (number == sumOfSplit)
   where
-  sumOfSplit = (uncurry (+)) $ split (length . show $ number) (number^2)
+  digitsInNumber = length . show $ number
+  sumOfSplit = (uncurry (+)) $ split digitsInNumber (number^2)
 
 -- | return Kaprekar Numbers in a given range
 -- > kaprekarNumbers 1 10 == [1,9]
@@ -32,7 +35,7 @@ kaprekarNumbers from to = filter isKaprekar [from .. to]
 pairify :: [Int] -> (Int, Int)
 pairify (x:y:_) = (x, y)
 
--- | formatted output
+-- | format output
 putResult :: [Int] -> IO ()
 putResult rs
   | null rs = putStrLn "INVALID RANGE"
