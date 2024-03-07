0d08f85d30995d59eb82a2c9d455c0861c3db79c
RawIron <manianology@gmail.com>
2024-03-06 20:48:54 -0800

    30 days of code - day 11 py -  fix naming, get ready for tests

--- 30days-of-code/11_arrays2d.py
+++ 30days-of-code/11_arrays2d.py
@@ -1,32 +1,42 @@
 #!/bin/python3
 
 
-def generateSubMatrices(matrix, dim, sub_dim):
-    def createSubM(row_idx, column_idx):
-        subM = []
+def generate_sub_matrices(matrix, dim, sub_dim):
+    def create_subm(row_idx, column_idx):
+        subm = []
         for i in range(row_idx, row_idx+sub_dim):
-            subM.append(matrix[i][column_idx:column_idx+sub_dim])
-        return subM
+            subm.append(matrix[i][column_idx:column_idx+sub_dim])
+        return subm
 
     for row in range(dim-sub_dim+1):
         for column in range(dim-sub_dim+1):
-            yield createSubM(row, column)
+            yield create_subm(row, column)
 
 
-def calcHourGlassSum(matrix,dim):
-    sumMiddle = 0
+def calc_hourglass_sum(matrix,dim):
+    sum_middle = 0
     for i in range(1,dim-1):
-        sumMiddle += matrix[i][dim//2]
-    return sum(matrix[0]) + sumMiddle + sum(matrix[dim-1])
+        sum_middle += matrix[i][dim//2]
+    return sum(matrix[0]) + sum_middle + sum(matrix[dim-1])
 
 
-def generateFixedLines(n):
-    # generator objects are not subscriptable !!
-    # my_generator_list[4:] will fail at runtime
+def solve(matrix):
+    max_sum = ((2 * 3) + ((3-2) * 1)) * (-9) - 1
+    for sub_matrix in generate_sub_matrices(matrix,6,3):
+        sum_hourglass = calc_hourglass_sum(sub_matrix,3)
+        max_sum = max(sum_hourglass, max_sum)
+    return max_sum
+
+
+def generate_fixed_lines(n):
+    ''' generator objects are not subscriptable !!
+        my_generator_list[4:] will fail at runtime
+    '''
     for _ in range(n):
         yield map(int, input().rstrip().split())
 
-def readFixedLines(n):
+
+def read_fixed_lines(n):
     arr = []
     for _ in range(n):
         arr.append(list(map(int, input().rstrip().split())))
@@ -34,11 +44,5 @@ def readFixedLines(n):
 
 
 if __name__ == '__main__':
-    matrix = readFixedLines(6)
-
-    maxSum = ((2 * 3) + ((3-2) * 1)) * (-9) - 1
-    for subMatrix in generateSubMatrices(matrix,6,3):
-        sumHourGlass = calcHourGlassSum(subMatrix,3)
-        maxSum = max(sumHourGlass, maxSum)
-
-    print(maxSum)
+    solution = solve( read_fixed_lines(6) )
+    print(solution)
