5676aa80bfb7315f732a8514b559bc529715807c
RawIron <manianology@gmail.com>
2024-03-07 15:29:47 -0800

    challenges : mini max sum : ex : refactor

--- challenges/mini_max_sum.ex
+++ challenges/mini_max_sum.ex
@@ -1,23 +1,28 @@
-defmodule MiniMaxSum do
+defmodule MiniMax do
+  def sum_take4(numbers) do
+      import Enum, only: [take: 2, sum: 1]
 
-  defmodule MiniMax do
-      def sum_take4(numbers) do
-          import Enum, only: [take: 2, sum: 1]
+      is_sorted = Enum.sort numbers
 
-          is_sorted = Enum.sort numbers
-
-          {is_sorted |> take(4) |> sum,
-           is_sorted |> take(-4) |> sum}
-      end
+      {is_sorted |> take(4) |> sum,
+       is_sorted |> take(-4) |> sum}
   end
 
-  defmodule Solution do
-      result = IO.read(:stdio, :line)
-      |> String.split()
-      |> Enum.map(&String.to_integer(&1))
-      |> MiniMax.sum_take4()
-
-      :io.format("~B ~B", Tuple.to_list(result))
+  def read_input() do
+    IO.read(:stdio, :line)
+    |> String.split()
+    |> Enum.map(&String.to_integer(&1))
   end
 
+  def show(mini_max) do
+    :io.format("~B ~B", Tuple.to_list(mini_max))
+  end
+end
+
+
+defmodule Main do
+  import MiniMax
+  read_input()
+  |> sum_take4()
+  |> show()
 end
