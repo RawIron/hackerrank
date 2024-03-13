e879d49062693e7b5dfde3441ef75691e732af5b
RawIron <manianology@gmail.com>
2024-03-13 14:29:21 -0700

    challenges: repeated string : add a show function

--- challenges/repeated_string.py
+++ challenges/repeated_string.py
@@ -9,7 +9,7 @@ def count_repeats(word, n):
     in a repeated string
     repeat the string up to length of n
     
-    'aba' repeats for length 7 are 'abaabaa'
+    'aba' repeated to a length of 7 is 'abaabaa'
     count_repeats('aba', 7) == 5
     '''
     word_len = len(word)
@@ -17,15 +17,21 @@ def count_repeats(word, n):
     reminder = n % word_len
     return (words * word.count('a') + word[:reminder].count('a'))
 
+
+def show(result):
+    fileno = os.environ.get('OUTPUT_PATH', sys.stdout.fileno())
+    with open(fileno, 'w') as out:
+        out.write(str(result) + '\n')
+
+
 def main():
     s = input()
     n = int(input().strip())
 
     result = count_repeats(s, n)
-    
-    fileno = os.environ.get('OUTPUT_PATH', sys.stdout.fileno())
-    with open(fileno, 'w') as out:
-        out.write(str(result) + '\n')
+
+    show(result)
+
 
 if __name__ == '__main__':
     main()
