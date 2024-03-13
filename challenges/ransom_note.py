9f500a4487ad0fd107b71a3a65d65eff57c1905f
RawIron <manianology@gmail.com>
2024-03-13 11:26:12 -0700

    challenges : ransom note : add comment for check_magazine

--- challenges/ransom_note.py
+++ challenges/ransom_note.py
@@ -4,16 +4,35 @@ from collections import Counter
 
 
 def check_magazine(magazine, note):
-    if Counter(note) & Counter(magazine) == Counter(note):
+    """
+    true :  every word in note can be paired
+            with the same word in magazine
+
+    note            magazine
+                    time
+    it              it
+    is              is
+    time            time    
+    is              is
+                    over
+
+    >>> check_magazine(["time", "is", "is"], ["time", "is"]) == True
+    >>> check_magazine(["time", "is"], ["is", "is"]) == False
+    """
+    return Counter(note) & Counter(magazine) == Counter(note)
+
+
+def main():
+    _, _ = map(int, input().split())
+
+    magazine = input().rstrip().split()
+    note = input().rstrip().split()
+
+    if check_magazine(magazine, note):
         print("Yes")
     else:
         print("No")
 
 
 if __name__ == '__main__':
-    m, n = map(int, input().split())
-
-    magazine = input().rstrip().split()
-    note = input().rstrip().split()
-
-    check_magazine(magazine, note)
+    main()
