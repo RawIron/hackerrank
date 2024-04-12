576624732292589247354d53616a790a601b39ec
RawIron <manianology@gmail.com>
2024-04-11 18:20:02 -0700

    Revert "interview preparation : count triplets : memoization for function count_greater"
    This reverts commit 3ee5f162b55dd8a895a7084240482e94e96bbff7.

--- interview-preparation/dicts_count_triplets.cpp
+++ interview-preparation/dicts_count_triplets.cpp
@@ -43,14 +43,6 @@ void show(const T value) {
 
 /*
     store each item with a list of its indices
-
-    complexity
-        1 sequential read of n elements
-        n writes, append to vector
-        n searches on RB tree
-        ?? writes to RB tree
-        n vector initializations
-        ?? vector re-allocations
 */
 map<long,vector<int>> positions(const vector<long>& items) {
     map<long,vector<int>> pos{};
@@ -65,15 +57,8 @@ map<long,vector<int>> positions(const vector<long>& items) {
 }
 
 /*
-
-    complexity, average
-        1 sequential read of m/2 elements from smaller
-        1 sequential read of n elements from greater
-        n/2 append writes to vector
-        ?? vector re-allocations
 */
-vector<pair<int,int>>
-count_greater(const vector<int>& smaller, const vector<int>& greater) {
+vector<pair<int,int>> count_greater(const vector<int>& smaller, const vector<int>& greater) {
     vector<pair<int,int>> greater_counts{};
 
     auto counter{0};
@@ -84,34 +69,29 @@ count_greater(const vector<int>& smaller, const vector<int>& greater) {
             ++s_it;
         }
         if (counter > 0) {
-            greater_counts.push_back( make_pair(g, counter) );
+            greater_counts.push_back( make_pair(g,counter) );
         }
     }
 
     return greater_counts;
 }
 
-pair< vector<pair<int,int>>, vector<pair<int,int>> >
-count_greater(const vector<pair<int,int>>& smaller, const vector<int>& greater) {
+vector<pair<int,int>> count_greater(const vector<pair<int,int>>& smaller, const vector<int>& greater) {
     vector<pair<int,int>> greater_counts{};
-    auto counter{0};
-    vector<pair<int,int>> greater_memo{};
-    auto memo_counter{0};
 
+    auto counter{0};
     auto s_it{ smaller.begin() };
     for (auto g: greater) {
         while (s_it != smaller.end() && s_it->first < g) {
             counter += s_it->second;
-            ++memo_counter;
             ++s_it;
         }
         if (counter > 0) {
-            greater_counts.push_back( make_pair(g, counter) );
-            greater_memo.push_back( make_pair(g, memo_counter) );
+            greater_counts.push_back( make_pair(g,counter) );
         }
     }
 
-    return make_pair(greater_counts, greater_memo);
+    return greater_counts;
 }
 
 /*
@@ -126,12 +106,13 @@ long count_triplets(const vector<long>& numbers, const long r) {
     if (r > 31622) {
         // r^2 > 10^9
         // 1 <= numbers <= 10^9
-        // last element of any triplet can't be found in the list
+        // last element of the triplet can't be found in the list
         return 0L;
     }
 
     long total{0};
     const auto s{ positions(numbers) };
+    const auto max_number{ (*s.rbegin()).first };
 
     if (r == 1) {
         for (auto [_, idx_list]: s) {
@@ -142,38 +123,20 @@ long count_triplets(const vector<long>& numbers, const long r) {
          }
     }
 
-    const auto max_number{ (*s.rbegin()).first };
-    map< long, vector<pair<int,int>> > memo{};
-
     if (r > 1) {
         for (auto [elem, pos]: s) {
             const auto elem_r_squared = elem * r * r;
             if (elem_r_squared > max_number) {
-                // last element of triplet is not in the list
                 continue;
             }
-
             const triple triplet{ elem, elem * r, elem_r_squared};
             if (s.contains(triplet[1]) && s.contains(triplet[2])) {
-                pair< vector<pair<int,int>>, vector<pair<int,int>> > triplet_agg{};
-
-                if (memo.contains(triplet[1])) {
-                    // 2 4 8 16
-                    // 2 4 8
-                    //   4 8 16
-                    triplet_agg = count_greater(memo.at(triplet[1]), s.at(triplet[2]));
-                }
-                else {
-                    triplet_agg = count_greater(
-                                    count_greater(pos, s.at(triplet[1])),
-                                    s.at(triplet[2]));
-
-                }
-
-                for (auto&& t: triplet_agg.first) {
+                const auto triplet_agg = count_greater(
+                                        count_greater(pos, s.at(triplet[1])),
+                                        s.at(triplet[2]));
+                for (auto&& t: triplet_agg) {
                     total += t.second;
                 }
-                memo.insert(make_pair(triplet[2], triplet_agg.second));
             }
         }
     }
@@ -205,25 +168,20 @@ void test_count_triplets() {
     const long expected{ 6 };
     assert(count_triplets(have, 2) == expected);
     }
-    { // r == 1
+    {
     const vector<long> have{ 1, 1, 1, 1, 3, 3 };
     const long expected{ 4 };
     assert(count_triplets(have, 1) == expected);
     }
-    { // r > 31622
+    {
     const vector<long> have{ 1, 1, 1, 1, 3, 3 };
     const long expected{ 0 };
     assert(count_triplets(have, 33000) == expected);
     }
-    { // r^2 > max_element
+    {
     const vector<long> have{ 3, 6, 58, 36 };
     const long expected{ 0 };
     assert(count_triplets(have, 12) == expected);
-    }
-    { // memoization
-    const vector<long> have{ 2, 4, 8, 16 };
-    const long expected{ 2 };
-    assert(count_triplets(have, 2) == expected);
     }
 }
 
