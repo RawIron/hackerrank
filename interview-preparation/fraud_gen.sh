2975b76c0d4f310e2bc2ab4944e46d73fbadf45c
RawIron <manianology@gmail.com>
2024-04-19 13:46:55 -0700

    interview preparation : fraudulent activity notifications : script to generate random input

---
+++ interview-preparation/fraud_gen.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# generates test data for the FraudulentActivityNotifications challenge
+#
+# ./prison_gen.sh 100 100 | ./FraudulentActivityNotifications
+# ./prison_gen.sh | ./FraudulentActivityNotifications
+
+n=100000
+d=100
+expense=200
+
+if [[ $1 ]]; then
+    n="$1"
+fi
+
+if [[ $2 ]]; then
+    d="$2"
+fi
+
+echo "$n $d"
+
+for _ in $(seq 1 $n); do
+    echo -n "$((1 + $SRANDOM % $expense)) "
+done
