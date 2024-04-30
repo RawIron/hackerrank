3755bb71f8b8485d574e8192401908917cd52a52
RawIron <manianology@gmail.com>
2024-04-30 14:24:39 -0700

    interview preparation : candies : improve readability

--- interview-preparation/candies.py
+++ interview-preparation/candies.py
@@ -1,49 +1,7 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
 import sys
-
-
-def candies(n, scores):
-    '''
-    '''
-    candies_total = 0
-    previous_score = 0
-    latest_peak = 0
-    down_streak = 0
-
-    for score in scores:
-        if score > previous_score and down_streak > 0:
-            if down_streak >= latest_peak:
-                candies_total += down_streak - latest_peak + 1
-            down_streak = 0
-            latest_peak = 2
-            candies_total += latest_peak
-        elif score > previous_score and down_streak == 0:
-            latest_peak += 1
-            candies_total += latest_peak
-        elif score == previous_score and down_streak > 0:
-            if down_streak >= latest_peak:
-                candies_total += down_streak - latest_peak + 1
-            down_streak = 0
-            latest_peak = 1
-            candies_total += 1
-        elif score == previous_score and down_streak == 0:
-            latest_peak = 1
-            candies_total += 1
-        else:
-            down_streak += 1
-            candies_total += down_streak
-        previous_score = score
-
-    if down_streak > 0:
-        if down_streak >= latest_peak:
-            candies_total += down_streak - latest_peak + 1
-
-    return candies_total
 
 
 def show(result):
@@ -52,7 +10,7 @@ def show(result):
         out.write(str(result) + '\n')
 
 
-def main():
+def read_input():
     n = int(input())
 
     scores = []
@@ -60,10 +18,93 @@ def main():
         score = int(input())
         scores.append(score)
 
-    result = candies(n, scores)
+    return scores
 
+
+def candies(scores):
+    '''
+    use the minimum count of candies such that
+    of any neighbors the one with a higher score
+    receives more candies
+
+        0 0 0 1 0 0 0 1 2 3     down streak
+
+        0 1 2 2 0 1 2 2 2 2     up streak
+                    .
+                   / \
+            .     .   .
+           / \   /     \        scores
+          .   . .       .
+         /               \
+        .                 .
+        1 2 3 1 1 2 4 3 2 1     candies
+    '''
+    candies_total = 1
+    previous_score = scores[0]
+    up_streak = 0
+    down_streak = 0
+
+    for score in scores[1:]:
+        if score > previous_score and down_streak > 0:
+            #  .
+            #   \
+            #    .   .
+            #     \./
+            #  6 3 2 3
+            if down_streak > up_streak:
+                candies_total += down_streak - up_streak
+            down_streak = 0
+            up_streak = 1
+            candies_total += 2
+        elif score > previous_score and down_streak == 0:
+            #      .
+            #     /
+            #    .
+            #   /
+            #  2 4 8
+            up_streak += 1
+            candies_total += 1 + up_streak
+        elif score == previous_score and down_streak > 0:
+            #  .
+            #   \
+            #    .
+            #     \. .
+            #  6 3 2 2
+            if down_streak > up_streak:
+                candies_total += down_streak - up_streak
+            down_streak = 0
+            up_streak = 0
+            candies_total += 1
+        elif score == previous_score and down_streak == 0:
+            #    . .       . . .
+            #   /
+            #  2 4 4       4 4 4
+            up_streak = 0
+            candies_total += 1
+        else:
+            #  .          .
+            #   \        / \
+            #    .      .   .
+            #  6 3        6 3
+            down_streak += 1
+            candies_total += down_streak
+        previous_score = score
+
+    if down_streak > 0:
+        if down_streak > up_streak:
+            candies_total += down_streak - up_streak
+
+    return candies_total
+
+
+def main():
+    scores = read_input()
+    result = candies(scores)
     show(result)
 
 
 if __name__ == '__main__':
+    '''
+    a clean global namespace
+    '''
     main()
