c06219e9d85445160769605f6e3162995696abc0
RawIron <manianology@gmail.com>
2024-05-15 12:10:32 -0700

    interview preparation : fraudulent activity notifications : add max expense amount as argument to fraud_gen

--- interview-preparation/fraud_gen.sh
+++ interview-preparation/fraud_gen.sh
@@ -2,12 +2,12 @@
 #
 # generates test data for the FraudulentActivityNotifications challenge
 #
-# ./fraud_gen.sh 100 100 | ./FraudulentActivityNotifications
+# ./fraud_gen.sh 1000 100 | ./FraudulentActivityNotifications
 # ./fraud_gen.sh | ./FraudulentActivityNotifications
 
-n=100000
-d=100
-expense=200
+n=100000            # number of total expenses
+d=100               # window size used for fraud detection
+max_expense=200     # max amount for any expense
 
 if [[ $1 ]]; then
     n="$1"
@@ -17,8 +17,12 @@ if [[ $2 ]]; then
     d="$2"
 fi
 
+if [[ $3 ]]; then
+    max_expense="$3"
+fi
+
 echo "$n $d"
 
 for _ in $(seq 1 $n); do
-    echo -n "$((1 + $SRANDOM % $expense)) "
+    echo -n "$((1 + $SRANDOM % $max_expense)) "
 done
