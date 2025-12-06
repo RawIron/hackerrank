dc05025b109f13b670053e56503533059bbe456e
RawIron <manianology@gmail.com>
2025-12-05 18:05:17 -0800

    cpp skill : other preprocessor : make macro names uppercase

--- cpp-skills/other_preprocessor.cpp
+++ cpp-skills/other_preprocessor.cpp
@@ -1,13 +1,13 @@
 #include<limits>
 
-#define foreach(list, idx) for (size_t idx{0}; idx<list.size(); ++idx)
+#define FOREACH(list, idx) for (size_t idx{0}; idx<list.size(); ++idx)
 #define _TO_STR_IMPL(x) #x
-#define toStr(...) _TO_STR_IMPL(__VA_ARGS__)
-#define io(list) cin >> list 
+#define TO_STR(...) _TO_STR_IMPL(__VA_ARGS__)
+#define IO(list) cin >> list
 #define FUNCTION(func_name, compare) void func_name(int& extreme, const int value) { if (value compare extreme) { extreme = value; } }
 #define INF (numeric_limits<int>::max() - 1);
 
-#if !defined toStr || !defined io || !defined foreach || !defined FUNCTION || !defined INF
+#if !defined TO_STR || !defined IO || !defined FOREACH || !defined FUNCTION || !defined INF
 #   error "Missing preprocessor definitions"
 #endif
 
@@ -27,19 +27,19 @@ int main() {
     cin >> n;
 
     vector<int> v(n);
-    foreach(v, i) {
-        io(v)[i];
+    FOREACH(v, i) {
+        IO(v)[i];
     }
 
     int mn = INF;
     int mx = -INF;
-    foreach(v, i) {
+    FOREACH(v, i) {
         minimum(mn, v[i]);
         maximum(mx, v[i]);
     }
 
     const int range{ mx - mn };
-    cout << toStr(Result =) << ' ' << range;
+    cout << TO_STR(Result =) << ' ' << range << endl;
 
     return EXIT_SUCCESS;
 }
