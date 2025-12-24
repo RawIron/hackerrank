a8bc2322554bd6d5c3dd83897906a4eee5fd9155
RawIron <manianology@gmail.com>
2025-12-24 10:01:13 -0800

    functional : string permute : hs : clean code

--- functional/StringPermute.hs
+++ functional/StringPermute.hs
@@ -1,17 +1,24 @@
 module Main where
 
+-- | swap characters in each pair
+-- | concat characters into a string
+-- >  solve [(a,b), (c,d)] == "badc"
 solve :: [(Char,Char)] -> String
 solve = foldr (\(x,y) acc -> y:x:acc) []
 
+-- | convert a list to a list of pairs
+-- >  slicePairs [a,b,c,d] == [(a,b), (c,d)]
 slicePairs :: [a] -> [(a, a)]
-slicePairs = go where
+slicePairs = go
+  where
   go (x:y:xs) = (x,y) : go xs
   go [] = []
   go _ = []
 
+
 input :: IO [String]
 input = do
-  n <- readLn :: IO Int
+  _ <- readLn :: IO Int
   getContents >>= return . words
 
 main :: IO ()
